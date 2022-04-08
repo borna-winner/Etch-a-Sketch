6e2c7d416d612dba4ebec9dc792e2f407c03e14a
james <james@whygee.com>
2022-04-08 14:48:26 -0400

    finished grid and centered.
    Woohoo!

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const body = document.body
-const div = document.createElement("div")
+////make gridContainer useable, 
+const gridContainer = document.querySelector("#gridContainer"); 
+const body = document.body;
 function createGrid(){
-    for(let i = 0; i<16; i++){
-        document.createElement("div")
-        div.setAttribute('id', 'blockGrid');
-        body.append(div);
+    for(let i = 0; i<255; i++){    
+    ////create div with id and classname on variable(div)
+        let divBlock = document.createElement('div');
+        divBlock.id = 'blockGrid';
+        divBlock.className = 'block';
+        ///make div a child of gridContainer
+        gridContainer.append(divBlock);
     }
 }
 createGrid()        
