3349314de1510fe3e5629fd2afd1268af84ffd54
james <james@whygee.com>
2022-04-13 19:01:04 -0400

    grid changes color properly ! major success.

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-// eslint-disable-next-line prettier/prettier
-// make gridContainer useable, 
-const gridContainer = document.querySelector("#gridContainer"); 
-const divColor = document.querySelectorAll("#divBlock");
-
+const gridContainer = document.querySelector("#gridContainer");
 function createGrid(){
     for(let i = 0; i<255; i += 1){    
     // create div with id and classname on variable(div)
-        const divBlock = document.createElement('div');
-        divBlock.id = 'blockGrid';
-        divBlock.className = 'colorpad';
+        let divBlock = document.createElement('div');
+        divBlock.className = 'blockGrid';
         gridContainer.append(divBlock);
-    }
+}
 }
 createGrid()
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i += 1) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  
-function setRandomColor() {
-    for(let i = 0; i <255; i += 1){
 
-        let colorPad = document.getElementsByName("colorpad").css("background-color", getRandomColor());
+function addEvent(){
+  const squares = document.querySelectorAll('.blockGrid');
+  squares.forEach(squares => squares.addEventListener('mouseover', changeColor));
 
-    }
+
+
+}
+addEvent()
+
+let elements = document.getElementsByClassName('blockGrid');
+
+
+for (let i = 0; i < elements.length; ++i) {
+ elements[i].addEventListener('mouseover', changeColor)  
 }
 
-divColor.addEventListener("mouseover",(setRandomColor()));
-
-
+function changeColor() {
+    this.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
+}
+let color = [, "#3C9EE7", "#E7993C",
+            "#E73C99", "#3CE746", "#E7993C"];
